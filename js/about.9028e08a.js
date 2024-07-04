1742329fde1759ca39be751e2f6e945847947e06
Ajesh Madhavan <ajesh.madhavan@vonnue.com>
2024-07-04 09:48:06 +0530

    deploy files

---
+++ js/about.9028e08a.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkuser_details"]=self["webpackChunkuser_details"]||[]).push([[594],{999:function(s,t,u){u.r(t),u.d(t,{default:function(){return i}});var e=function(){var s=this;s._self._c;return s._m(0)},n=[function(){var s=this,t=s._self._c;return t("div",{staticClass:"about"},[t("h1",[s._v("This is an about page")])])}],a=u(1656),l={},r=(0,a.A)(l,e,n,!1,null,null,null),i=r.exports}}]);
+//# sourceMappingURL=about.9028e08a.js.map
